bef3cae981d26f974bd95b808e1069be0a234039
ity4sh <itsy4sh.dev@gmail.com>
2025-08-21 23:08:11 +0530

    base layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import localFont from 'next/font/local';
 import { Toaster } from '@/components/ui/sonner';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const Nacelle = localFont({
+  src: '../public/fonts/Nacelle-Regular.woff2',
+  variable: '--font-Nacelle',
 });
 
 export const metadata: Metadata = {
@@ -25,11 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${Nacelle.variable} antialiased`}>
         <Toaster />
+        {children}
       </body>
     </html>
   );
