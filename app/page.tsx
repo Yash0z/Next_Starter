97fb753258e487d956a67847d2c604aaf91dec7d
ity4sh <itsy4sh.dev@gmail.com>
2025-08-22 23:19:40 +0530

    themeswitcher

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
+import ThemeSwitcher from '@/components/misc/themeswitcher';
 export default function Home() {
   return (
     <main className='flex h-full flex-col justify-between'>
       <div className='flex h-full w-full justify-between text-5xl/loose'>
-        <div className='w-full border-r pt-3 pl-10'>
-          <h1 className='text-7xl/tight'>Heading-72</h1>
+        <div className='mt-3 w-full pl-10'>
+          <h1 className='font-display text-7xl/tight'>Heading-72</h1>
           <h1 className='text-3xl/loose'>Heading-30</h1>
-          <h1 className='text-base/loose'>Heading-24</h1>
+          <h1 className='font-mono text-lg/loose'>Heading-18</h1>
+          <h1 className='font-mono text-base/loose'>Heading-16</h1>
         </div>
-        <div className='w-full'></div>
+
+        <div className='pr-5'>
+          <ThemeSwitcher />
+        </div>
       </div>
       <div className='h-full bg-secondary pl-3 text-5xl/loose text-secondary-foreground'>
         ready
