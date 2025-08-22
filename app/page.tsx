81578976b8f09525115228698bdc77314a6900d8
ity4sh <itsy4sh.dev@gmail.com>
2025-08-22 22:07:17 +0530

    Color & Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
 export default function Home() {
   return (
-    <div className='flex h-full flex-col justify-between'>
-      <div className='h-full bg-black font-monospace'>Twhat is that</div>
-      <div className='h-full bg-white font-geist-mono'>Twhat is that</div>
-    </div>
+    <main className='flex h-full flex-col justify-between'>
+      <div className='flex h-full w-full justify-between text-5xl/loose'>
+        <div className='w-full border-r pt-3 pl-10'>
+          <h1 className='text-7xl/tight'>Heading-72</h1>
+          <h1 className='text-3xl/loose'>Heading-30</h1>
+          <h1 className='text-base/loose'>Heading-24</h1>
+        </div>
+        <div className='w-full'></div>
+      </div>
+      <div className='h-full bg-secondary pl-3 text-5xl/loose text-secondary-foreground'>
+        ready
+      </div>
+    </main>
   );
 }
